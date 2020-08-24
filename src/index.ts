bd7fd0fd07fa004cd17117b39e3158bffda2d4a1
Jai Sharma <jaiprakash.sharma44@gmail.com>
2020-08-25 00:17:17 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,10 @@ app.get('/', async (req: Request, res: Response) => {
 });
 
 app.listen(8000, () => {
-    console.log('App is running at http://localhost:%d in %s mode', 8000);
+    console.log(
+        'App is running at https://jplofttechnologies.in:%d in %s mode',
+        8000
+    );
 });
 
 //define host and port to run the server
